4a26dc7d63794cdecda7962be9f376941ec71df1
KLN\tharukas <tharukavishwajiths@gmail.com>
2021-11-05 19:54:33 +0530

    Finished view student details message dialog

--- lib/model/student_model.dart
+++ lib/model/student_model.dart
@@ -2,7 +2,12 @@ class StudentResponse {
   late int id;
   late String studentNo;
   late String nicNo;
-  late String year;
+  late String firstName;
+  late String lastName;
+  late int year;
+  late String faculty;
+  late String department;
+  late bool isSuspended;
   late String? uniEmail;
   late String? imageUrl;
   late String? imageDataBase64;
@@ -11,7 +16,12 @@ class StudentResponse {
     required this.id,
     required this.studentNo,
     required this.nicNo,
+    required this.firstName,
+    required this.lastName,
     required this.year,
+    required this.faculty,
+    required this.department,
+    required this.isSuspended,
     this.uniEmail,
     this.imageUrl,
     this.imageDataBase64
@@ -22,7 +32,12 @@ class StudentResponse {
       id: json["id"] != null ? json["id"] : null,
       studentNo: json["studentNo"] != null ? json["studentNo"] : "",
       nicNo: json["nicNo"] != null ? json["nicNo"] : "",
-      year: json["year"] != null ? json["year"] : "",
+      firstName: json["firstName"] != null ? json["firstName"] : "",
+      lastName: json["lastName"] != null ? json["lastName"] : "",
+      year: json["year"] != null ? json["year"] : 0,
+      faculty: json["faculty"] != null ? json["faculty"] : "",
+      department: json["department"] != null ? json["department"] : "",
+      isSuspended: json["suspended"] != null ? json["suspended"] : 0,
       uniEmail: json["uniEmail"] != null ? json["uniEmail"] : "",
       imageUrl: json["imageUrl"] != null ? json["imageUrl"] : "",
       imageDataBase64: json["imageData"] != null ? json["imageData"] : "",
@@ -34,7 +49,12 @@ class StudentResponse {
       'id': id,
       'studentNo': studentNo.trim(),
       'nicNo': nicNo.trim(),
+      'firstName': firstName.trim(),
+      'lastName': lastName.trim(),
       'year': year,
+      'faculty': faculty.trim(),
+      'department': department.trim(),
+      'isSuspended': isSuspended,
       'uniEmail': uniEmail!.trim(),
       'imageUrl': imageUrl!.trim(),
       'imageDataBase64': imageDataBase64!.trim()
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -9,7 +9,8 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:sams/util/image_util.dart';
 
 class HomeScreen extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of your application
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -41,49 +42,59 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   Barcode? result;
-  late QRViewController controller;
+  QRViewController? controller;
+  bool isDetailsDialogOpened = false;
 
   @override
   void reassemble() {
     super.reassemble();
     if (Platform.isAndroid) {
-      controller.pauseCamera();
+      controller!.pauseCamera();
     } else if (Platform.isIOS) {
-      controller.resumeCamera();
+      controller!.resumeCamera();
     }
   }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // void _incrementCounter() {
+  //   setState(() {
+  //     // This call to setState tells the Flutter framework that something has
+  //     // changed in this State, which causes it to rerun the build method below
+  //     // so that the display can reflect the updated values. If we changed
+  //     // _counter without calling setState(), then the build method would not be
+  //     // called again, and so nothing would appear to happen.
+  //     // _counter++;
+  //   });
+  // }
 
   void _onQRViewCreated(QRViewController controller) {
     this.controller = controller;
-    controller.scannedDataStream.listen((scanData) async {
+    controller.scannedDataStream.listen((scanData) {
+      if (scanData != null) {
+        isDetailsDialogOpened = true;
+        controller.pauseCamera();
+      }
       setState(() {
         result = scanData;
       });
-      APIService().getStudent('s001').then((value) {
+      APIService().getStudent('SE-2013-038').then((value) {
         print(value.studentNo + '--------' + value.nicNo);
         _studentDetailDialog(context, value);
       });
-      await controller.pauseCamera();
-
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    if (controller != null && !this.isDetailsDialogOpened) {
+      controller!.resumeCamera();
+      print('==========' +
+          isDetailsDialogOpened.toString() +
+          '====================');
+      print(
+          '==================================resumeCamera=============================');
+    }
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -115,20 +126,20 @@ class _MyHomePageState extends State<MyHomePage> {
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[Expanded(
-            flex: 5,
-            child: QRView(
-              key: qrKey,
-              onQRViewCreated: _onQRViewCreated,
+          children: <Widget>[
+            Expanded(
+              flex: 5,
+              child: QRView(
+                key: qrKey,
+                onQRViewCreated: _onQRViewCreated,
+              ),
             ),
-          ),
             Expanded(
               flex: 1,
               child: Center(
                 child: (result != null)
                     ? Text(
-                    'Barcode Type: ${describeEnum(
-                        result!.format)}   Data: ${result!.code}')
+                        'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}')
                     : Text('Scan a code'),
               ),
             )
@@ -170,34 +181,124 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   _studentDetailDialog(context, StudentResponse student) {
-    return showDialog(context: context, builder: (context) {
-      return  AlertDialog(
-        title: Text("Connect to new server"),
-        content: Container(
-          // child: Image.asset('assets/images/login_bottom.png'),
-          child: new ImageUtil().imageFromBase64String(student.imageDataBase64),
-        ),
-        actions: <Widget>[
-          MaterialButton(
-            elevation: 5.0,
-            child: Text("Connect"),
-            onPressed: () async {
-              await controller.resumeCamera();
-              Navigator.pop(context,false);
-              Fluttertoast.showToast(
-                  msg: "Camera resumed!",
-                  toastLength: Toast.LENGTH_SHORT,
-                  gravity: ToastGravity.CENTER,
-                  timeInSecForIosWeb: 1,
-                  backgroundColor: Colors.red,
-                  textColor: Colors.white,
-                  fontSize: 16.0
-              );
-            },
-          )
-        ],
-      );
-    });
+    return showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            // title: Text(student.studentNo, textAlign: TextAlign.center),
+            content: Container(
+              // child: Image.asset('assets/images/login_bottom.png'),
+              // child: new ImageUtil().imageFromBase64String(student.imageDataBase64),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  // ImageUtil().imageFromBase64String(student.imageDataBase64),
+                  Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.green,
+                      width: 5,
+                    )),
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: ImageUtil()
+                          .imageFromBase64String(student.imageDataBase64),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Text(
+                        student.firstName + ' ' + student.lastName,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.normal, fontSize: 30),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Text(
+                        student.studentNo,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.normal, fontSize: 20),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Text(
+                        'LEVEL ' + student.year.toString(),
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.normal, fontSize: 20),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Text(
+                        'FACULTY OF ' + student.faculty.toTitleCase,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.normal, fontSize: 20),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              Container(
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: MaterialButton(
+                      color: Colors.green,
+                      elevation: 5.0,
+                      child: Text(
+                        "Verify",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () async {
+                        await controller!.resumeCamera();
+                        isDetailsDialogOpened = false;
+                        Navigator.pop(context, false);
+                        Fluttertoast.showToast(
+                            msg: "Camera resumed!",
+                            toastLength: Toast.LENGTH_SHORT,
+                            gravity: ToastGravity.CENTER,
+                            timeInSecForIosWeb: 1,
+                            backgroundColor: Colors.red,
+                            textColor: Colors.white,
+                            fontSize: 16.0);
+                      },
+                    ),
+                  )
+              ),
+            ],
+          );
+        });
   }
+}
 
+extension StringCasingExtension on String {
+  String toCapitalized() =>
+      this.length > 0 ? '${this[0].toUpperCase()}${this.substring(1)}' : '';
+
+  String get toTitleCase => this
+      .replaceAll(RegExp(' +'), ' ')
+      .split(" ")
+      .map((str) => str.toCapitalized())
+      .join(" ");
 }
